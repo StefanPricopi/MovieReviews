9936137d46bfd5ee4764e47be20f86d9b4166e53
Pricopi,Ştefan Ş.A <s.pricopi@student.fontys.nl>
2023-10-03 12:44:17 +0200

    Added media class and class library

---
+++ IndividualS2/LogicLayerClassLibrary/Classes/Media.cs
@@ -0,0 +1,27 @@
+﻿using LogicLayerClassLibrary.Enums;
+namespace LogicLayerClassLibrary.Classes
+
+{
+    public class Media
+    {
+        
+        private int Id { get; }
+        private string Title { get; set; }
+        private string Director{ get; set; }
+        private string Actor { get; set; }
+        private string Description {  get; set; }
+        
+        private Genre Genre {  get; set; }
+
+        public Media(int id,string title,string director,string actor,string description,Genre genre)
+        {
+            Id = id;
+            Title = title;
+            Director = director;
+            Actor = actor;
+            Description = description;
+            Genre = genre;
+            
+        }
+    }
+}
