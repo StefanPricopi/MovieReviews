570d4a238ead4c830b8274e7ebd06ffdcd921765
Pricopi,Ştefan Ş.A <s.pricopi@student.fontys.nl>
2023-11-14 14:42:05 +0100

    more unit test

---
+++ IndividualS2/TestSuite/MovieUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using LogicLayerClassLibrary.Enums;
+using ModelLibrary.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSuite
+{
+    [TestClass]
+    public class MovieUnitTest
+    {
+        [TestMethod]
+        public void AddMovie()
+        {
+            // Arrange
+            var media = new Dictionary<int, MediaDTO>();
+            var movies = new Dictionary<int, MovieDTO>();
+
+            var testMovieDAL = new TESTMovieDAL(media, movies);
+
+            var validMediaDTO = new MediaDTO { Id = 1, Title="abra",Director="cadabra",Actor="jesu",Description="dtest",Genre=Genre.SF};
+            var validMovieDTO = new MovieDTO { Id=1, Duration=120, Date = new DateTime(2003, 2, 12) };
+
+            // Act
+            bool result = testMovieDAL.AddMovie(validMediaDTO, validMovieDTO);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.IsTrue(testMovieDAL.media.ContainsKey(validMediaDTO.Id));
+            Assert.AreEqual(validMediaDTO, testMovieDAL.media[validMediaDTO.Id]);
+            Assert.IsTrue(testMovieDAL.movies.ContainsKey(validMediaDTO.Id));
+            Assert.AreEqual(validMovieDTO, testMovieDAL.movies[validMediaDTO.Id]);
+        }
+        
+    }
+}
---
+++ IndividualS2/TestSuite/TESTMovieDAL.cs
@@ -0,0 +1,58 @@
+﻿using DALClassLibrary.DALs;
+using DALClassLibrary.Interfaces;
+using ModelLibrary.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSuite
+{
+    public class TESTMovieDAL : IMovieManagerDAL
+    {
+        public Dictionary<int, MediaDTO> media;
+        public Dictionary<int, MovieDTO> movies;
+        public TESTMovieDAL(Dictionary<int, MediaDTO> media, Dictionary<int, MovieDTO> movies) 
+        {
+            this.media = media ?? new Dictionary<int, MediaDTO>();
+            this.movies = movies ?? new Dictionary<int, MovieDTO>();
+        }
+        public bool AddMovie(MediaDTO mediaDTO, MovieDTO movieDTO)
+        {
+            if (mediaDTO != null && movieDTO != null && mediaDTO.Id > 0)
+            {
+                if (media.ContainsKey(mediaDTO.Id))
+                {
+                    return false;
+                }
+                else
+                {
+                    media.Add(mediaDTO.Id, mediaDTO);
+                    movies.Add(mediaDTO.Id, movieDTO);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public DataTable GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SearchMovies(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateMovie(MediaDTO mediaDTO, MovieDTO movieDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
